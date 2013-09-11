14e9f64e071f5e6eb0715b979ef6bc0eed3ade97
jty <jtyjty99999@126.com>
2013-09-11 21:41:07 +0800

    firstCommit

---
+++ app.js
@@ -0,0 +1,22 @@
+﻿var express = require('express')
+  , app = express()
+  , url = require('url')
+  , path = require('path');
+  app.configure(function(){
+  app.set('port', process.env.PORT || 3000);//
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());//解析客户端请求
+  app.use(express.methodOverride());
+  app.use(express.static(path.join(__dirname, 'public')));//静态文件支持
+});
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');//利用express做跳转
+});
+app.get('/query',function(req, res){
+  var queryObj = url.parse(req.url,true)
+  console.log(queryObj.search)
+  res.send({"message": "test"});  
+});  
+app.listen(8888);
+
